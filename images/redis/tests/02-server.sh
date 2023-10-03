4323fe84a2d1dea19ecc8b33cf271418cbc6fb1e
James Rawlings <jrawlings@chainguard.dev>
2023-10-03 10:59:33 +0100

    wip
    Signed-off-by: James Rawlings <jrawlings@chainguard.dev>

--- images/redis/tests/02-server.sh
+++ images/redis/tests/02-server.sh
@@ -8,7 +8,7 @@ CONTAINER_NAME=${CONTAINER_NAME:-"redis-smoketest-$(date +%s)-$(uuidgen)"}
 # Run two containers on the same network (just use host for simplicity)
 # The server should start up and listen, and the client will run ping
 
-docker run -d -p ${FREE_PORT}:6379 --name $CONTAINER_NAME $IMAGE_NAME
+docker run --platform linux/aarch64 -d -p ${FREE_PORT}:6379 --name $CONTAINER_NAME $IMAGE_NAME
 
 trap "docker logs $CONTAINER_NAME && docker rm -f $CONTAINER_NAME && rm ${CONTAINER_NAME}_${FREE_PORT}.txt" EXIT
 
